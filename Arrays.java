fc59e3b259fe7cfa1eee0ddd76935b20c6b3ac4b
wahedpro <wahedulislam.pro@gmail.com>
2023-01-04 18:56:04 +0600

    array input and output

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Arrays{
     public static void main(String[] ages){
-        System.out.println("hello world");
+        //create array
+        int[] number=new int[10];
+        
+        //Scanner Object create
+        Scanner input=new Scanner(System.in);
+
+        //input value in array 
+        for(int i=0; i<number.length; i++){
+            number[i]=input.nextInt();
+        }
+
+        //Array print
+        for (int i=0; i<number.length; i++){
+            System.out.print(number[i]+" ");
+        }
     }
 }
